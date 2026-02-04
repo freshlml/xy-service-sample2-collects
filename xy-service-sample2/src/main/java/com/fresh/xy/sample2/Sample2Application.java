8b98534013e5730f47a24e95928f1be6ad4feedd
freshlml <1326191992@qq.com>
2026-02-04 22:59:54 +0800

    1

--- xy-service-sample2/src/main/java/com/fresh/xy/sample2/Sample2Application.java
+++ xy-service-sample2/src/main/java/com/fresh/xy/sample2/Sample2Application.java
@@ -11,7 +11,7 @@ import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan(basePackages = {"com.fresh.xy.sample2", "com.fresh.core", "com.fresh.xy.common", /*"com.fresh.xy.redis",*/ "com.fresh.xy.sample.api"})
+@ComponentScan(basePackages = {"com.fresh.xy.sample2", "com.fresh.xy.common", /*"com.fresh.xy.redis",*/ "com.fresh.xy.sample.api"})
 @EnableCircuitBreaker
 @EnableHystrixDashboard
 @EnableFeignClients({"com.fresh.xy.sample.api"})
